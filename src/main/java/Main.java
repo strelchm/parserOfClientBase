422158d5b3a4ab7dd787187b64bde17596a54941
Michael Strelchenko <strelchm@yandex.ru>
2022-01-31 02:02:00 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         //String inputValue = "+7 926 123 45 67 8(926)123-45-67";
-        String inputValue = "muzei-kodinsk@mail.ru Мария Ивановна (391) 211 36 35, Тел.: (391) 211 36 35" +
-                "lib@kraslib.ru  ;  39199-21389 Дарина Грино   (391) 211 36 35; Парам Парам (391) 224 56 11";
+        String inputValue = "some data";
 
         /**
          WorkerRecord workerRecord = new WorkerRecord(inputValue);
